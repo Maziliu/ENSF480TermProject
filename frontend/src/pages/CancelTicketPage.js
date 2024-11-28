abc77693eeb20d6079829e9e2941ee4ff2760374
miri-j-kim <miri.kim@ucalgary.ca>
2024-11-28 15:58:14 -0700

    updated layout of seatmap

--- frontend/src/pages/CancelTicketPage.js
+++ frontend/src/pages/CancelTicketPage.js
@@ -58,7 +58,7 @@ const CancelTicketPage = () => {
           />
         </label> <br/>
         <label>
-          Cancellation Code:
+          Booking ID:
           <input
             type="text"
             value={transactionId}
